8ab6339b3c55e86b28a32a3cebb752102a7bc54d
Leevi Vesa <joulumieli2@gmail.com>
2018-02-04 20:49:53 +0200

    Versio 1

--- App.js
+++ App.js
@@ -1,23 +1,125 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text , FlatList, View, Button } from 'react-native';
+import MapView from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+const LATITUDE = 60.1699;
+const LONGITUDE = 24.9384;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBa1PSCHdZ74voA-BJTF1GGtRvNg_GuaLs';
+
+class Example extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // AirBnB's Office, and Apple Park
+        this.state = {
+            coordinates: [
+                {
+                    latitude: 60.1699,
+                    longitude: 24.9384,
+                },
+                {
+                    latitude:  60.2933,
+                    longitude: 25.040,
+                },
+            ],
+            data: []
+        };
+
+        this.mapView = null;
+    }
+    componentWillMount(){
+        this.fetchData();
+    }
+    //hakee tms dataa.
+    fetchData = async () => {
+        const response =  await fetch("https://tie.digitraffic.fi/api/v1/data/tms-data?lastUpdated=false");
+        const json = await response.json();
+        this.setState({data: json.tmsStations});
+    };
+
+
+    onMapPress = (e) => {
+        this.setState({
+            coordinates: [
+                ...this.state.coordinates,
+                e.nativeEvent.coordinate,
+            ],
+        });
+    }
+
+    render() {
+        function onPressLearnMore(){
+            alert("moi pelle");
+        };
+        return (
+            <View>
+                <MapView
+                    initialRegion={{
+                        latitude: LATITUDE,
+                        longitude: LONGITUDE,
+                        latitudeDelta: LATITUDE_DELTA,
+                        longitudeDelta: LONGITUDE_DELTA,
+                    }}
+                    style={{ alignSelf: 'stretch', height: 250, marginTop: 25, }}
+                    ref={c => this.mapView = c}
+                    onPress={this.onMapPress}
+                    showsUserLocation={true}
+                    followsUserLocation={true}
+                    showsMyLocationButton={true}
+                    customMapStyle={mapStyle}
+                >
+                    {this.state.coordinates.map((coordinate, index) =>
+                        <MapView.Marker key={`coordinate_${index}`} coordinate={coordinate} />
+                    )}
+                    {(this.state.coordinates.length >= 2) && (
+                        <MapViewDirections
+                            origin={this.state.coordinates[0]}
+                            waypoints={ (this.state.coordinates.length > 2) ? this.state.coordinates.slice(1, -1): null}
+                            destination={this.state.coordinates[this.state.coordinates.length-1]}
+                            apikey={GOOGLE_MAPS_APIKEY}
+                            strokeWidth={3}
+                            strokeColor="hotpink"
+                            onReady={(result) => {
+                                this.mapView.fitToCoordinates(result.coordinates, {
+                                    edgePadding: {
+                                        right: (width / 20),
+                                        bottom: (height / 20),
+                                        left: (width / 20),
+                                        top: (height / 20),
+                                    }
+                                });
+                            }}
+                            onError={(errorMessage) => {
+                                // console.log('GOT AN ERROR');
+                            }}
+                        />
+                    )}
+                </MapView>
+                <Button
+                    onPress={onPressLearnMore}
+                    title="NAPPI"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+                <FlatList  data={this.state.data}
+                           keyExtractor={(x, i) => i}
+                           renderItem={({ item })=>
+                               <Text>
+                                   {`id:${item.id} tmsNumber:${item.tmsNumber} sensorValue:${item.sensorValues[0].id}`}
+                               </Text>}
+
+                />
+            </View>
+        );
+    }
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+mapStyle = [ { "elementType": "geometry", "stylers": [ { "color": "#1d2c4d" } ] }, { "elementType": "labels.text.fill", "stylers": [ { "color": "#8ec3b9" } ] }, { "elementType": "labels.text.stroke", "stylers": [ { "color": "#1a3646" } ] }, { "featureType": "administrative.country", "elementType": "geometry.stroke", "stylers": [ { "color": "#4b6878" } ] }, { "featureType": "administrative.land_parcel", "elementType": "labels.text.fill", "stylers": [ { "color": "#64779e" } ] }, { "featureType": "administrative.province", "elementType": "geometry.stroke", "stylers": [ { "color": "#4b6878" } ] }, { "featureType": "landscape.man_made", "elementType": "geometry.stroke", "stylers": [ { "color": "#334e87" } ] }, { "featureType": "landscape.natural", "elementType": "geometry", "stylers": [ { "color": "#023e58" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#283d6a" } ] }, { "featureType": "poi", "elementType": "labels.text.fill", "stylers": [ { "color": "#6f9ba5" } ] }, { "featureType": "poi", "elementType": "labels.text.stroke", "stylers": [ { "color": "#1d2c4d" } ] }, { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [ { "color": "#023e58" } ] }, { "featureType": "poi.park", "elementType": "labels.text.fill", "stylers": [ { "color": "#3C7680" } ] }, { "featureType": "road", "elementType": "geometry", "stylers": [ { "color": "#304a7d" } ] }, { "featureType": "road", "elementType": "labels.text.fill", "stylers": [ { "color": "#98a5be" } ] }, { "featureType": "road", "elementType": "labels.text.stroke", "stylers": [ { "color": "#1d2c4d" } ] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [ { "color": "#2c6675" } ] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "color": "#255763" } ] }, { "featureType": "road.highway", "elementType": "labels.text.fill", "stylers": [ { "color": "#b0d5ce" } ] }, { "featureType": "road.highway", "elementType": "labels.text.stroke", "stylers": [ { "color": "#023e58" } ] }, { "featureType": "transit", "elementType": "labels.text.fill", "stylers": [ { "color": "#98a5be" } ] }, { "featureType": "transit", "elementType": "labels.text.stroke", "stylers": [ { "color": "#1d2c4d" } ] }, { "featureType": "transit.line", "elementType": "geometry.fill", "stylers": [ { "color": "#283d6a" } ] }, { "featureType": "transit.station", "elementType": "geometry", "stylers": [ { "color": "#3a4762" } ] }, { "featureType": "water", "elementType": "geometry", "stylers": [ { "color": "#0e1626" } ] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [ { "color": "#4e6d70" } ] } ]
+export default Example;
