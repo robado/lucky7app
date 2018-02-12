e2dfb5b19f70262ef06e867f9e1e0302381eed93
Robert Ridal <ridal.robert@gmail.com>
2018-02-12 22:57:53 +0200

    Navigointi testiesimerkki

--- App.js
+++ App.js
@@ -3,21 +3,65 @@ import { Dimensions, Text , FlatList, View, Button, AppRegistry, StyleSheet, Lis
 import AppHeader from './src/components/appHeader';
 import AppBody from './src/components/appBody';
 import AppFooter from './src/components/appFooter';
-import {AppMap} from './src/components/appMap';
+import { AppMap } from './src/components/appMap';
 import AppJson from "./src/components/appJson"
+import Expo from 'expo';
+import { StackNavigator } from 'react-navigation';
 
-
-export default class lucky7app extends Component{
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Home',
+    };
     render() {
+        const { navigate } = this.props.navigation;
         return (
             <View>
-                <AppHeader/>
-                <AppMap/>
-                <AppBody/>
-                <AppJson/>
-                <AppFooter/>
+                <Button
+                    title = "Navigate to Profile"
+                    onPress= { ()=> navigate('Profile') }>
+                </Button>
+                
             </View>
         )
     }
 }
+
+class ProfileScreen extends React.Component {
+    static navigationOptions = {
+        title: 'ProfileScreen',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View>
+                <Button
+                    title = " Navigate to Home"
+                    onPress= { ()=> navigate('Home') }>
+                </Button>
+                <AppJson/>
+            </View>
+        )
+    }
+}
+
+const NavigationApp = StackNavigator({
+    Home: {screen: HomeScreen},
+    Profile: {screen: ProfileScreen},
+}, {
+    navigationOptions: {
+        headerStyle: {
+            marginTop: Expo.Constants.statusBarHeight
+        }
+    }
+})
+
+export default class lucky7app extends React.Component{
+    render() {
+        return (
+            <NavigationApp />
+        )
+    }
+}
+
+
         AppRegistry.registerComponent('lucky7app',()=> 'lucky7app');
