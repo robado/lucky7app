c3788956213ba559b1b85ab7ea6df1dd6c72ff66
Sasu Ryynälä <32301466+sasuryynala@users.noreply.github.com>
2018-04-17 16:27:49 +0300

    Uusi branch. Kameroiden tulostus. Loopilla tulostamisen yritys.

--- src/components/appCamera.js
+++ src/components/appCamera.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { View, Image, Dimensions } from 'react-native';
 
 
-const bottom = "25%";
+var x;
+const window = Dimensions.get('window');
+
 export default class AppCamera extends Component {
     constructor(props) {
         super(props);
@@ -14,20 +16,46 @@ export default class AppCamera extends Component {
             .then((response) => response.json())
             .then((responseData) => {
 
+
+
                 let numero = 6;
-                let camera = 0;
+                var camera;
+                var cameraId = [];
+                for(let i = 0; i < responseData.cameraStations[numero].cameraPresets.length; i++) {
+                    camera = responseData.cameraStations[numero].cameraPresets[i].id;
+                    cameraId.push(camera);
+                }
+
+                //console.log(cameraId);
+
+                for (x = 0; x < cameraId.length - 1; x++){
+
+                }
+                console.log(x);
+
+
                 this.setState({
-                    imgId: (responseData.cameraStations[numero].cameraPresets[camera].id)
+                    imgId: (cameraId)
                 });
+
+
             });
+
     }
+
 
     render() {
 
-        return <View>
-            <Image style={{bottom: bottom, width: Dimensions.get('window').width, height: Dimensions.get('window').height, resizeMode: 'contain'}} source={{uri: 'https://weathercam.digitraffic.fi/' + this.state.imgId + '.jpg' + '?' + new Date()}}/>
+        return <View style={{flex: 1}}>
+            <Image style={{width: window.width, height: 300}}
+                   source={{uri: 'https://weathercam.digitraffic.fi/' + this.state.imgId[x] + '.jpg' + '?' + new Date()}}/>
+
+
         </View>;
     }
+
+
+
 }
 module.exports = AppCamera;
 
